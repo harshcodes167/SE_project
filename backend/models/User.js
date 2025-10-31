1fd2f6d8cff23432d6287e2366c87bbd1fd05d26
Lavanya595 <lavanya050905@gmail.com>
2025-10-31 19:50:24 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -50,7 +50,6 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
     next();
@@ -60,7 +59,6 @@ userSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
